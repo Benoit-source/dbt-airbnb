4bc623e4bd59671d2a8c9075a9ae13eda4d837ae
Benoit-source <Benoit-source@users.noreply.github.com>
2025-12-04 14:15:02 +0000

    deploy: 3954e4acf9c4af0cd8844c0fe1b884448e09ea84

---
+++ compiled/dbt_airbnb/models/staging/_staging__models.yml/not_null_stg_airbnb__listing_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from AIRBNB_BI.BI_BRONZE.stg_airbnb__listing
+where id is null
+
+
---
+++ compiled/dbt_airbnb/models/staging/_staging__models.yml/not_null_stg_airbnb__review_listing_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select listing_id
+from AIRBNB_BI.BI_BRONZE.stg_airbnb__review
+where listing_id is null
+
+
