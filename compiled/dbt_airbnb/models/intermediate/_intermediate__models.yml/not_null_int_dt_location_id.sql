8a9fa7e5dcbddbc899ca57e8fe1f09c5f8db367c
Benoit-source <Benoit-source@users.noreply.github.com>
2026-01-30 10:42:36 +0000

    deploy: 2af99725a62ee1ff85e4b12e23a437fdd6b3c6e6

---
+++ compiled/dbt_airbnb/models/intermediate/_intermediate__models.yml/not_null_int_dt_location_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from AIRBNB_BI.BI_SILVER.int_dt_location
+where id is null
+
+
---
+++ run/dbt_airbnb/models/intermediate/int_dt_location.sql
@@ -0,0 +1,59 @@
+
+        
+
+    
+        create dynamic table AIRBNB_BI.BI_SILVER.int_dt_location
+        target_lag = '24 hour'
+        warehouse = COMPUTE_WH
+        refresh_mode = AUTO
+
+        initialize = ON_CREATE
+
+        
+
+        
+
+        
+
+        as (
+            
+
+with loc as (
+Select ID, 
+	Case When PRICE <= 50 Then 1
+         When PRICE <= 100 Then 2
+         When PRICE <= 200 Then 3
+         When PRICE <= 500 Then 4
+         When PRICE <= 1000 Then 5
+        Else 6
+        End CODE_PRICE,
+	Case When ROOM_TYPE = 'Entire home/apt' Then 1
+		When ROOM_TYPE = 'Private room' Then 2
+		When ROOM_TYPE = 'Shared room' Then 3
+		When ROOM_TYPE = 'Hotel room' Then 4
+		End CODE_ROOM_TYPE,
+    Case When VILLE = 'Bordeaux' Then 1
+		 When VILLE = 'Lyon' Then 2
+		 When VILLE = 'Paris' Then 3
+		 When VILLE = 'Pays Basque' Then 4
+	End CODE_VILLE,
+	LATITUDE,
+	LONGITUDE,
+	NUMBER_OF_REVIEWS,
+	REVIEW_SCORES_ACCURACY,
+	REVIEW_SCORES_CHECKIN,
+	REVIEW_SCORES_CLEANLINESS,
+	REVIEW_SCORES_COMMUNICATION,
+	REVIEW_SCORES_LOCATION,
+	REVIEW_SCORES_RATING,
+	REVIEW_SCORES_VALUE
+From AIRBNB_BI.BI_BRONZE.stg_airbnb__listing
+Where FG_DER_VER = 1)
+
+select * from loc
+        )
+
+    
+
+
+    
