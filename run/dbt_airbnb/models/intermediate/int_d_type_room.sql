eb7b4f39762d3d2d57b2afa9e4bda20897e2fafb
Benoit-source <Benoit-source@users.noreply.github.com>
2026-02-09 12:43:23 +0000

    deploy: d033db15ab05d1341f74ad18f5a2edd4c498635e

---
+++ run/dbt_airbnb/models/intermediate/int_d_type_room.sql
@@ -0,0 +1,39 @@
+
+  create or replace   view AIRBNB_BI.BI_SILVER.int_d_type_room
+  
+    
+    
+(
+  
+    "CODE_ROOM_TYPE" COMMENT $$$$, 
+  
+    "ROOM_TYPE" COMMENT $$$$
+  
+)
+
+  
+  
+  
+  as (
+    with listing as (
+
+    select Distinct ROOM_TYPE from AIRBNB_BI.BI_BRONZE.stg_airbnb__listing
+
+),
+
+room as (
+
+    select Distinct 
+    Case When ROOM_TYPE = 'Entire home/apt' Then 1
+        When ROOM_TYPE = 'Private room' Then 2
+        When ROOM_TYPE = 'Shared room' Then 3
+        When ROOM_TYPE = 'Hotel room' Then 4
+        End CODE_ROOM_TYPE,
+    ROOM_TYPE 
+    from listing
+
+)
+
+select * from room
+  );
+
