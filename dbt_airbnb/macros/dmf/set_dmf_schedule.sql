6f7dfe8c4b73ba0170fa2f206c099032f7dee195
Benoit-source <benoitottavi@yahoo.fr>
2026-02-19 16:39:58 +0100

    Suite test DMF

--- dbt_airbnb/macros/dmf/set_dmf_schedule.sql
+++ dbt_airbnb/macros/dmf/set_dmf_schedule.sql
@@ -1,6 +1,6 @@
 {% macro set_dmf_schedule(table_name, schedule) %}
 
-ALTER TABLE {{ this.database }}.{{ this.schema }}.{{ table_name }}
+ALTER TABLE {this}
 SET DATA_METRIC_SCHEDULE = '{{ schedule }}';
 
 {% endmacro %}
